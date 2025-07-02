a3784fc9f2a95b5d726e08a21d1a4db71cc040f0
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-07-02 19:30:15 -0300

    em progresso

--- node.js
+++ node.js
@@ -1,11 +1,48 @@
-function gerarSenha(tamanho) {
-    const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let senhaGerada = '';
-    for (let i=0; i<tamanho;i++) {
-        const randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length);
-        senhaGerada += caracteresUsados[randomizadorSenha];
+let i = '', senhaGerada = '', randomizadorSenha = '', randomizadorSecundario = '';
+const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+
+let quote = 'Voce gostaria de uma senha com numeros e letras repetidas?';
+console.log(quote);
+let lineReader = require('lineReader');
+
+const rl = lineReader.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('quote', (answer) => {
+    console.log('Certo, sua resposta foi registrada!');
+    rl.close();
+});
+
+if (answer=='Sim' || answer=='sim') {
+    function gerarSenhaCondicionada(tamanho) {
+        senhaGerada = '';
+        for (i=0;i<tamanho;i++) {
+            randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length);
+            senhaGerada += caracteresUsados[randomizadorSenha];
+        }
+        for (i=0;i<tamanho;i++) {
+            randomizadorSecundario = Math.floor(Math.random()*caracteresUsados.length/2);
+            senhaGerada += caracteresUsados[randomizadorSecundario];
+        }
+
     }
-    return senhaGerada;
+} else {
+    function gerarSenha(tamanho) {
+        senhaGerada = '';
+        for (i=0; i<tamanho;i++) {
+            randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length);
+            senhaGerada += caracteresUsados[randomizadorSenha];
+        }
+        for (i=0;i<tamanho;i++) {
+            randomizadorSecundario = Math.floor(Math.random()*caracteresUsados.length/2);
+            senhaGerada += caracteresUsados[randomizadorSecundario];
+        }
+        return senhaGerada;
+    }
 }
+
 
 console.log(gerarSenha(10));
--- script.js
+++
@@ -1,9 +0,0 @@
-function gerarSenha(pão) {
-    const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&+-*=§¢£';
-    let senhaGerada = '';
-    for (let i=0; i<tamanho;i++) {
-        const randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length);
-        senhaGerada += caracteresUsados[randomizadorSenha];
-    }
-    return senhaGerada;
-}
