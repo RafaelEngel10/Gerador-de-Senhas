0d6aff8c088c1a05a8b85b3fbd9e9a9a43e86007
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-07-02 20:48:54 -0300

    testando uns coiso

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
-let i = '', senhaGerada = '', randomizadorSenha = '', randomizadorSecundario = '';
-const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+let i = '', senhaGerada = '', randomizadorSenha = '';
+const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&§¢£';
 
 let quote = 'Voce gostaria de uma senha com numeros e letras repetidas?';
 const answer = require('readline');
@@ -11,23 +11,19 @@ const rl = answer.createInterface ({
 function gerarSenha(tamanho) {
     senhaGerada = '';
     for (i=0; i<tamanho;i++) {
-        randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length);
+        randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length/tamanho);
         senhaGerada += caracteresUsados[randomizadorSenha];
     }
-    for (i=0;i<tamanho;i++) {
-        randomizadorSecundario = Math.floor(Math.random()*caracteresUsados.length/2);
-        senhaGerada += caracteresUsados[randomizadorSecundario];
-    }
-    return senhaGerada
+    return senhaGerada;
 }
 
 rl.question("Quantos caracteres a senha deve ter? ", (input) => {
-  const tamanho = parseInt(input);
-  if (!isNaN(tamanho) && tamanho>0) {
-    const senha = gerarSenha(tamanho);
-    console.log(`Sua senha gerada e: ${senha}`);
-  } else {
-    console.log("Por favor, insira um numero valido maior que zero.");
-  }
-  rl.close();
+    const tamanho = parseInt(input);
+    if (!isNaN(tamanho) && tamanho>0) {
+        const senha = gerarSenha(tamanho);
+        console.log(`Sua senha gerada e: ${senha}`);
+    } else {
+        console.log("Por favor, insira um numero valido maior que zero.");
+    }
+    rl.close();
 });
