29312f02474e6985b33f2ba048581cc1788558c1
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-07-03 10:59:43 -0300

    Gerador de senha concertado com sucesso!

--- node.js
+++ node.js
@@ -1,25 +1,25 @@
 let i = '', senhaGerada = '', randomizadorSenha = '';                                                   //variáveis a serem usadas inicializadas com nada
-const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&§¢£';        //constante de caracteres para criação de senha
+const caracteresUsados = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!#$%&§?';        //constante de caracteres para criação de senha
 
-let quote = 'Voce gostaria de uma senha com numeros e letras repetidas?';                               //variável armazenando string para pergunta
 const answer = require('readline');                                                              //variável armazenando função require('readline') que lê oque o usuário digita
+const { callbackify } = require('util');
 const rl = answer.createInterface ({                                                         //variável de input e output atraves da criação de interface
     input: process.stdin,
     output: process.stdout
 });
 
 function gerarSenha(tamanho) {                                                                  //função para gerar a senha conforme o parâmetro 'tamanho'
-    senhaGerada = '';
     for (i=0; i<tamanho;i++) {                                                                 //laço de repetição que percorre toda a variável caracteresUsados
         randomizadorSenha = Math.floor(Math.random()*caracteresUsados.length);               //randomizador que escolhe uma posição aleatória e determina se vai ser usada ou não
-        senhaGerada += caracteresUsados[randomizadorSenha];                                 //atribuição da posição usada para dentro da variável
+        senhaGerada = senhaGerada + caracteresUsados[randomizadorSenha];                                 //atribuição da posição usada para dentro da variável
     }
     return senhaGerada;                                                                   //retorna a variável com a senha gerada
 }
 
 rl.question("Quantos caracteres a senha deve ter? ", (input) => {                       //função question e output para fazer a pergunta, input para entrada de dados
     const tamanho = parseInt(input);                                                   //constante de tamanho atribuida a função parseInt que transforma a string em um numero
-    if (!isNaN(tamanho) && tamanho>0) {                                           
+    if (!isNaN(tamanho) && tamanho>0) {       
+        senhaGerada = gerarSenha(tamanho);                                            //tive que chamar a função de volta                                    
         console.log(`Sua senha gerada e: ${senhaGerada}`);                            //mostra a senha no terminal
     } else {
         console.log("Por favor, insira um numero valido maior que zero.");            //caso o usuário seja engraçadinho e bote -20 caracteres
